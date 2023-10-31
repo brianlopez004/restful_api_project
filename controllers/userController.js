cc42dac0e74366ff039336b666a6df05fe8e0934
Braian López <braianortiz1603@gmail.com>
2023-10-30 21:23:23 -0500

    feat: Modify the delete from user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,79 +1,102 @@
-//Enlazamos nuestro servicio 
+//Enlazamos nuestro servicio
 // const { use } = require('../api/v1/routes/users.routes');
-const userService = require('../services/userService');
+const userService = require("../services/userService");
 
-//Funcion para obtener un todos los usuarios 
+//Funcion para obtener un todos los usuarios
 const getAllUsers = async (req, res) => {
-    try {
-        const allUsers = await userService.getAllUsers();
-        res.status(200).send({ status: "OK", data: allUsers })
-    } catch (error) {
-        res.status(error.status || 500).send({ status: "FAILED", data: { error: error.message } });
-    }
-}
+  try {
+    const allUsers = await userService.getAllUsers();
+    res.status(200).send({ status: "OK", data: allUsers });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", data: { error: error.message } });
+  }
+};
 
 //Funcion para obtener un usuario especifico
 const getUser = async (req, res) => {
-    try {
-        let id = req.params.UserId;
-        const user = await userService.getUser(id)
-        res.status(200).send({ status: "OK", data: user })
-    } catch (error) {
-        res.status(error.status || 500).send({ status: "FAILED", data: { error: error.message } });
-    }
-
-}
+  try {
+    let id = req.params.UserId;
+    const user = await userService.getUser(id);
+    res.status(200).send({ status: "OK", data: user });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", data: { error: error.message } });
+  }
+};
 
 //Funcion para crear un usuario especifico
 const createUser = async (req, res) => {
-    try {
-        const { name, email, password, phone } = req.body;
-        const missingFields = [];
+  try {
+    const { name, email, password, phone } = req.body;
+    const missingFields = [];
 
-        if (!name) missingFields.push('name');
-        if (!email) missingFields.push('email');
-        if (!phone) missingFields.push('phone');
-        if (!password) missingFields.push('password');
+    if (!name) missingFields.push("name");
+    if (!email) missingFields.push("email");
+    if (!phone) missingFields.push("phone");
+    if (!password) missingFields.push("password");
 
-        if (missingFields.length > 0) {
-            const errorMessage = `Missing required fields: ${missingFields.join(', ')}`;
-            return res.status(400).send({ status: 'failed', data: { error: errorMessage } });
-        }
-
-        const createdUser = await userService.createUser(name, email, password, phone);
-        res.status(200).send({ status: "OK", data: createdUser })
-    } catch (error) {
-        res.status(error.status || 500).send({ status: "FAILED", data: { error: error.message } });
+    if (missingFields.length > 0) {
+      const errorMessage = `Missing required fields: ${missingFields.join(
+        ", "
+      )}`;
+      return res
+        .status(400)
+        .send({ status: "failed", data: { error: errorMessage } });
     }
-}
+
+    const createdUser = await userService.createUser(
+      name,
+      email,
+      password,
+      phone
+    );
+    res.status(200).send({ status: "OK", data: createdUser });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", data: { error: error.message } });
+  }
+};
 
 //Funcion para modificar un usuario
 const updateUser = async (req, res) => {
-    try {
-        let id = req.params.UserId;
-        let { name, email, password, phone } = req.body;
-        const updatedUser = await userService.updateUser(id, name, email, password, phone);
-        res.status(200).send({ status: "OK", data: updatedUser })
-    } catch (error) {
-        res.status(error.status || 500).send({ status: "FAILED", data: { error: error.message } });
-    }
-}
+  try {
+    let id = req.params.UserId;
+    let { name, email, password, phone } = req.body;
+    const updatedUser = await userService.updateUser(
+      id,
+      name,
+      email,
+      password,
+      phone
+    );
+    res.status(200).send({ status: "OK", data: updatedUser });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", data: { error: error.message } });
+  }
+};
 
-//Funcion para eliminar un usuario 
+//Funcion para eliminar un usuario
 const deleteUser = async (req, res) => {
-    try {
-        let id = req.params.UserId;
-        const deletedUser = await userService.deleteUser(id)
-        res.status(200).send({ status: "OK", data: deletedUser })
-    } catch (error) {
-        res.status(error.status || 500).send({ status: "FAILED", data: { error: error.message } });
-    }
-}
+  const id = req.params.userId;
+  const deleteUser = await userService.deleteUser(id);
+
+  if (deleteUser) {
+    res.status(200).send({ status: "OK", data: deleteUser });
+  } else {
+    res.status(400).send({ status: "FAILED", data: null });
+  }
+};
 
 module.exports = {
-    getAllUsers,
-    getUser,
-    createUser,
-    updateUser,
-    deleteUser,
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
 };
