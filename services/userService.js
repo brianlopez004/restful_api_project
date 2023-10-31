1bc95e652f8b23d5e0d554e0c6037b6143e767f3
Braian López <braianortiz1603@gmail.com>
2023-10-30 21:21:33 -0500

    feat: User update is modified

--- services/userService.js
+++ services/userService.js
@@ -1,73 +1,75 @@
-const db = require('../models')
+const db = require("../models");
 
 const getAllUsers = async () => {
-    try {
-        let users = await db.User.findAll();
-        return users;
-    } catch (error) {
-        throw { status: 500, message: error.message || "failed load users" };
-    }
-}
+  try {
+    let users = await db.User.findAll();
+    return users;
+  } catch (error) {
+    throw { status: 500, message: error.message || "failed load users" };
+  }
+};
 
 const getUser = async (id) => {
-    try {
-        let user = await db.User.findByPk(id)
-        return user
-    } catch (error) {
-        throw { status: 500, message: error.message || "failed to get user" };
-    }
-}
+  try {
+    let user = await db.User.findByPk(id);
+    return user;
+  } catch (error) {
+    throw { status: 500, message: error.message || "failed to get user" };
+  }
+};
 
 const createUser = async (name, email, phone, password) => {
-    try {
-        let newUser = await db.User.create({
-            name,
-            email,
-            password,
-            phone
-        });
-        return newUser;
-    } catch (error) {
-        throw { status: 400, message: error.message || "failed to create user" };
-    }
-}
+  try {
+    let newUser = await db.User.create({
+      name,
+      email,
+      password,
+      phone,
+    });
+    return newUser;
+  } catch (error) {
+    throw { status: 400, message: error.message || "failed to create user" };
+  }
+};
 
 const updateUser = async (id, name, email, password, phone) => {
-    try {
-        let updatedUser = await db.User.update({
-            name,
-            email,
-            password,
-            phone
+  try {
+    let updatedUser = await db.User.update(
+      {
+        name,
+        email,
+        password,
+        phone,
+      },
+      {
+        where: {
+          id,
         },
-            {
-                where: {
-                    id,
-                }
-            });
-        return updatedUser;
-    } catch (error) {
-        throw { status: 500, message: error.message || "failed to update user" };
-    }
-}
+      }
+    );
+    return updatedUser;
+  } catch (error) {
+    throw { status: 500, message: error.message || "failed to update user" };
+  }
+};
 
 const deleteUser = async (id) => {
-    try {
-        let deletedUser = await db.User.destroy({
-            where: {
-                id,
-            }
-        });
-        return deletedUser
-    } catch (error) {
-        throw { status: 500, message: error.message || "failed to delete user" };
-    }
-}
+  try {
+    const deletedUser = await db.User.destroy({
+      where: {
+        id,
+      },
+    });
+    return deletedUser;
+  } catch (error) {
+    return error.message || "User could not be deleted";
+  }
+};
 
 module.exports = {
-    getAllUsers,
-    getUser,
-    createUser,
-    updateUser,
-    deleteUser,
-}
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
